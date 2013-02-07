9b89bb7d0280abb7b2c19293c4eb7cd9db263820
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-06 22:31:32 -0800

    re-enable async

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -3,7 +3,7 @@ class Subscriber
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :registerable, :database_authenticatable, :confirmable #, :async
+  devise :registerable, :database_authenticatable, :confirmable, :async
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
