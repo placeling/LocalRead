b5026d0d468648fb0122b8e84b7f5916431f42c2
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-06 17:35:01 -0800

    add some popup messages for confirmations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,6 +15,7 @@ class HomeController < ApplicationController
     @subscriber = Subscriber.new( :email => params[:subscriber][:email], :location => params[:subscriber][:location].split(","), :place_json => params[:subscriber][:place_json] )
 
     if @subscriber.save
+      track! :registration
       respond_to do |format|
         format.html { redirect_to :action => "dead_signup" }
       end
