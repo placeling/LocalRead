9ef2d8b38e367b3495609ed0cdd16bad86da5f0c
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-18 19:42:12 -0800

    basic file for kicking off subscriptions

---
+++ app/workers/process_weekly_emails.rb
@@ -0,0 +1,25 @@
+class ProcessWeeklyEmails
+  @queue = :email
+
+  def self.perform()
+
+    if Rails.env.production?
+      Subscriber.where(:loc => {"$near" => [49.263548,-123.114166] , '$maxDistance' =>1}).each do |subscriber|
+        if subscriber.weekly_email? && !subscriber.loc.nil? && subscriber.loc != [0.0, 0.0]
+          mail =  WeeklyMailer.thelocal( subscriber.id )
+          #mail.deliver
+        end
+      end
+    else
+      Subscriber.where(:loc => {"$near" => [49.263548,-123.114166] , '$maxDistance' =>1}).limit(3).each do |subscriber|
+        if subscriber.weekly_email? && !subscriber.loc.nil? && subscriber.loc != [0.0, 0.0]
+          mail = Notifier.weekly( subscriber.id )
+          #mail.deliver unless mail.to == nil don't actually deliver by default
+        elsif subscriber.weekly_email?
+          puts "skip #{user.username} because loc= #{subscriber.loc}"
+        end
+      end
+    end
+
+  end
+end
