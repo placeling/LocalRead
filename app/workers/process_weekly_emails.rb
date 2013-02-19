0f6cf2fd523a5f854f3f63236013bee83f84dfdb
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-19 15:18:34 -0800

    fixup how users are selected to get the emails

--- app/workers/process_weekly_emails.rb
+++ app/workers/process_weekly_emails.rb
@@ -13,7 +13,7 @@ class ProcessWeeklyEmails
     else
       Subscriber.where(:loc => {"$near" => [49.263548,-123.114166] , '$maxDistance' =>1}).limit(3).each do |subscriber|
         if subscriber.weekly_email? && !subscriber.loc.nil? && subscriber.loc != [0.0, 0.0]
-          mail = Notifier.weekly( subscriber.id )
+          mail = WeeklyMailer.thelocal( subscriber.id )
           #mail.deliver unless mail.to == nil don't actually deliver by default
         elsif subscriber.weekly_email?
           puts "skip #{user.username} because loc= #{subscriber.loc}"
