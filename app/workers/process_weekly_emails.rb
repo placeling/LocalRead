8ea442792da1555dd3cd8861ebc7c20ddcbfc097
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-22 10:48:53 -0800

    add a more stable version of the process kickoff

--- app/workers/process_weekly_emails.rb
+++ app/workers/process_weekly_emails.rb
@@ -4,19 +4,16 @@ class ProcessWeeklyEmails
   def self.perform()
 
     if Rails.env.production?
-      Subscriber.where(:loc => {"$near" => [49.263548,-123.114166] , '$maxDistance' =>1}).each do |subscriber|
-        if subscriber.weekly_email? && !subscriber.loc.nil? && subscriber.loc != [0.0, 0.0]
-          mail =  WeeklyMailer.thelocal( subscriber.id )
-          #mail.deliver
+      Subscriber.where(:location => {"$near" => [49.263548,-123.114166] , '$maxDistance' =>1}).each do |subscriber|
+        if subscriber.weekly_email?
+          WeeklyMailer.thelocal( subscriber.id ).deliver
         end
       end
     else
-      Subscriber.where(:loc => {"$near" => [49.263548,-123.114166] , '$maxDistance' =>1}).limit(3).each do |subscriber|
-        if subscriber.weekly_email? && !subscriber.loc.nil? && subscriber.loc != [0.0, 0.0]
-          mail = WeeklyMailer.thelocal( subscriber.id )
-          #mail.deliver unless mail.to == nil don't actually deliver by default
-        elsif subscriber.weekly_email?
-          puts "skip #{user.username} because loc= #{subscriber.loc}"
+      Subscriber.near(location: [49.263548,-123.114166]).each do |subscriber|
+        if subscriber.weekly_email?
+          puts subscriber.email
+          WeeklyMailer.thelocal( subscriber.id )
         end
       end
     end
