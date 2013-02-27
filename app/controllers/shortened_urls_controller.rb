d650720d1333f33fc312dd53faaf453aa1675f3e
imackinn <imackinn@alumni.uwaterloo.ca>
2013-02-26 17:41:16 -0800

    save increment

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -10,15 +10,11 @@ class ShortenedUrlsController < ApplicationController
 
     if sl
       # don't want to wait for the increment to happen, make it snappy!
-      # this is the place to enhance the metrics captured
-      # for the system. You could log the request origin
-      # browser type, ip address etc.
-      #Thread.new do
-        sl.use_count = sl.use_count + 1
-        s1.save
+      sl.use_count = sl.use_count + 1
+      sl.save
       #  ActiveRecord::Base.connection.close
       #end
-        request.env['HTTP_REFERER'] = "http://thelocalread.com"
+      request.env['HTTP_REFERER'] = "http://thelocalread.com"
       # do a 301 redirect to the destination url
       redirect_to sl.url, :status => :moved_permanently
     else
