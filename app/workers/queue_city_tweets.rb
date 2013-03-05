fd9341200e796c16419b85f11c26c9b916823d44
imackinn <imackinn@alumni.uwaterloo.ca>
2013-03-05 14:00:50 -0800

    put the "placed" twitter handles up front

--- app/workers/queue_city_tweets.rb
+++ app/workers/queue_city_tweets.rb
@@ -6,7 +6,7 @@ class QueueCityTweets
     City.each do |city|
       $redis.del( city.city_queue_key )
 
-      response = HTTParty.get("#{APP_CONFIG['chatham_location']}/recommendations/nearby.json?lat=#{city.location[0]}&lng=#{city.location[1]}")
+      response = HTTParty.get("#{APP_CONFIG['chatham_location']}/recommendations/nearby.json?lat=#{city.location[0]}&lng=#{city.location[1]}&since=#{3.days.ago.to_i}")
       chatham_data = Hashie::Mash.new( response )
       bloggers = chatham_data.bloggers
 
@@ -21,7 +21,16 @@ class QueueCityTweets
 
       all_entries.shuffle!
 
+      all_entries.sort_by!{|entry|
+        if (!entry[4].nil? &&  entry[4] !="")
+          0
+        else
+          1
+        end
+      }
+
       all_entries.each do |entry|
+        puts entry
         $redis.rpush( city.city_queue_key, entry.to_json )
       end
     end
