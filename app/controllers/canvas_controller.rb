837431d4b6244ae40f43526269efe0b602a2089c
imackinn <imackinn@alumni.uwaterloo.ca>
2013-03-07 10:41:21 -0800

    preseed email

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -15,10 +15,10 @@ class CanvasController < ApplicationController
 
         @subscriber = Subscriber.where(:email => email).first
 
-        if false #!@subscriber.nil?
+        if !@subscriber.nil?
           @invite = true
         else
-          @subscriber = Subscriber.new( :location => [49.261226, -123.1139268], :facebook_json => profile.to_json, :place_json=>"{\"address_components\":[{\"long_name\":\"Vancouver\",\"short_name\":\"Vancouver\",\"types\":[\"locality\",\"political\"]}" )
+          @subscriber = Subscriber.new( :email => email, :location => [49.261226, -123.1139268], :facebook_json => profile.to_json, :place_json=>"{\"address_components\":[{\"long_name\":\"Vancouver\",\"short_name\":\"Vancouver\",\"types\":[\"locality\",\"political\"]}" )
         end
       end
 
